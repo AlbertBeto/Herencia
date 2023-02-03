ff264f371244e37cd825ee2b2ced6385ebc2505a
Beto <beto@beto.cat>
2023-02-03 01:46:14 +0100

    Creado Main y realizados pruebas para el encapsulado

---
+++ src/EjerciciosComposicion/Main.java
@@ -0,0 +1,31 @@
+package EjerciciosComposicion;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Techo techo = new Techo(2,3);
+        Pared norte = new Pared("Norte");
+        Pared sur = new Pared("Sur");
+        Pared este = new Pared("Este");
+        Pared oeste = new Pared("Oeste");
+        Cama cama = new Cama("Tudor",4,70,3,2);
+        Lampara lampara = new Lampara("Tudor",false,4);
+
+
+        Habitacion invitados = new Habitacion("invitados",norte,este,sur,oeste,techo,cama,lampara);
+
+        invitados.hacerLaCama();
+        System.out.println();
+        System.out.println(lampara.hayBateria());
+        lampara.encender();
+
+
+        //mainmain
+    }
+
+
+
+    //class
+}
