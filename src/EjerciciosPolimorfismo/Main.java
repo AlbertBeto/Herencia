f852ecda533a8f044fd763ec05ee965d9f5a3f53
Beto <beto@beto.cat>
2023-02-05 03:50:03 +0100

    Primeros ejercicios de Polimorfismo.

---
+++ src/EjerciciosPolimorfismo/Main.java
@@ -0,0 +1,22 @@
+package EjerciciosPolimorfismo;
+
+public class Main {
+
+    public static void main(String[] args) {
+Tarjeta card = new Vacaciones("Amy");
+card.saludar();
+
+card = new SanValentin("Bob",3);
+card.saludar();
+
+
+
+
+
+
+
+    }
+
+
+    //class
+}
