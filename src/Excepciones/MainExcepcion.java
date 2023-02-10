8eef50cace8dafb1dfdb8c9224ced5e72f0d0e02
Albert <beto@beto.cat>
2023-02-10 16:47:13 +0100

    Creo Main de Excepciones y carpeta

---
+++ src/Excepciones/MainExcepcion.java
@@ -0,0 +1,18 @@
+package Excepciones;
+
+public class MainExcepcion {
+
+    public static void main(String[] args) {
+
+String texto = "Test";
+
+int divisor = Integer.parseInt("Mi divisor");
+
+        System.out.println(3/0);
+
+
+
+        //main
+    }
+    //class
+}
